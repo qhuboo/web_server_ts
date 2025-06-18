58d39d4a7df5d74a55afd6f91a500e576530c12e
qhuboo <joselukas24@gmail.com>
2025-06-17 22:48:07 -0500

    idk

--- src/callbackTCP.ts
+++ src/callbackTCP.ts
@@ -7,21 +7,21 @@ server.listen({ host: "127.0.0.1", port: 1234 });
 server.on("connection", onConnection);
 
 server.on("error", (error: Error): never => {
-    throw error;
+	throw error;
 });
 
 function onConnection(socket: net.Socket): void {
-    socket.on("data", (data: Buffer) => {
-        console.log("data:", data);
-        socket.write(data);
+	socket.on("data", (data: Buffer) => {
+		console.log("data:", data);
+		socket.write(data);
 
-        if (data.includes("q")) {
-            console.log("closing");
-            socket.end();
-        }
-    });
+		if (data.includes("q")) {
+			console.log("closing");
+			socket.end();
+		}
+	});
 
-    socket.on("end", () => {
-        console.log("EOF.");
-    });
+	socket.on("end", () => {
+		console.log("EOF.");
+	});
 }
